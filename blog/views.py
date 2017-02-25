91306e81a3f5734c9982279d4f79ac3e4c7c46fb
Rafael Santos <rsanthos@gmail.com>
2017-02-24 23:32:15 -0300

    criando primeiro post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from blog.models import Category
+from blog.models import Category, Post
 
 def home(request):
     name = "Rafael"
@@ -12,9 +12,23 @@ def home(request):
 
     all_categories = Category.objects.all()
 
+    category_python = Category.objects.get(name='PHP')
+
+    '''
+    post = Post()
+    post.name = 'My first post'
+    post.content = 'Content of my first post'
+    post.status = 'Published'
+    post.category = category_python
+    post.save()
+    '''
+
+    post = Post.objects.get(pk=1)
+
     context = {
         'name': name,
-        'categories': all_categories
+        'categories': all_categories,
+        'post': post,
     }
 
     return render(request, 'blog/home.html', context)
